a98f971a2ee4ad022084708632532f9e62973864
StrykerUX <abraham.stryker117@gmail.com>
2025-04-26 04:49:02 -0600

    Agregar sidebar.php - barra lateral con widgets

---
+++ sidebar.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * La barra lateral contiene los widgets
+ *
+ * @package NovaUI
+ */
+
+if ( ! is_active_sidebar( 'sidebar-1' ) ) {
+    return;
+}
+?>
+
+<aside id="secondary" class="widget-area">
+    <?php dynamic_sidebar( 'sidebar-1' ); ?>
+</aside><!-- #secondary -->
